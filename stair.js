e89b70cf17f3287eafeb573fb99404db0013def4
hoang <haong@gmail.com>
2024-06-15 05:29:00 +0800

    update stair.js file

--- stair.js
+++ stair.js
@@ -32,10 +32,10 @@ var climbStairs = function (n) {
         return n;
     }
 
-    const dp = new Array(n + 1).fill(0);
-    dp[1] = 1;
-    dp[2] = 2;
-    for (let i = 3; i <= n; i++) {
+    const dp = new Array(n);
+    dp[0] = 1;
+    dp[1] = 2;
+    for (let i = 2; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
     return dp[n];
