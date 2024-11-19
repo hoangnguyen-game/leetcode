300b7b3f2e91e9b067749ba0be3860cdc3b0b621
hoangdeptrai <haong@gmail.com>
2024-11-19 19:34:56 +0800

    twosum

--- twosum.js
+++ twosum.js
@@ -3,18 +3,86 @@
  * @param {number} target
  * @return {number[]}
  */
-export * from "./twosum.js";
-var twoSum = function(nums, target) {
-    let len = nums.length;
-
-    for (let i = 0; i < len; i++) {
-        for (let j = i + 1; j < len; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j];
-            }
-        }
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
+  }
+
+  return 1;
 };
 
-let arr2 = twoSum([3,2,4], 6);
-console.log(arr2);
+var twoSum2 = function (nums, target) {
+  const numMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let x = target - nums[i];
+
+    if (numMap.has(x)) {
+      return [numMap.get(x), i];
+    }
+
+    numMap.set(nums[i], i);
+  }
+};
+
+var twoSum3 = function (nums, target) {
+  const box = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let x = target - nums[i];
+
+    if (x in box) {
+      return [box[x], i];
+    }
+
+    box[nums[i]] = i;
+  }
+
+  return null;
+};
+
+let nums = [3, 2, 4],
+  target = 6;
+
+console.log(twoSum3(nums, target));
+// 1. Two Sum
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up
+//  to target.
+
+// You may assume that each input would have exactly one
+// solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
